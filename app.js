90e30b45b11053f72ea15629e34672aa9a003d89
etesongg <etesong0114@gmail.com>
2024-10-01 01:42:24 +0900

    로그인 회원가입 제외 기본 기능 개발 완료

--- app.js
+++ app.js
@@ -4,85 +4,155 @@ const PORT = 3000;
 
 const sqlite3 = require("sqlite3");
 const path = require("path");
-const db_name = path.join(__dirname, "habbit.db");
+const db_name = path.join(__dirname, "habit.db");
 const db = new sqlite3.Database(db_name);
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended: true}))
+app.engine("ejs", require("express-ejs-extend"));
+app.use(express.urlencoded({ extended: true }));
 
 const create_users_table = `
-    create table if not exists users (
-        id integer primary key autoincrement,
-        email varchar,
+    CREATE TABLE users(
+        id integer PRIMARY KEY AUTOINCREMENT,
         name varchar,
+        email varchar unique,
         password varchar,
-        createAt datetime current_timestamp,
-        unique(email)
-    );`
+        createAt datetime DEFAULT CURRENT_TIMESTAMP
+    );`;
 const create_habits_table = `
-    create table if not exists habits (
-        id integer primary key autoincrement,
-        name varchar,
+    CREATE TABLE habits (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        habit_name varchar,
         start_date datetime,
         end_date datetime,
-        createdAt datetime,
-        user_id integer,
-        foreign key(user_id) references users(id)
-    );`
+        createAt datetime DEFAULT CURRENT_TIMESTAMP,
+        user_id integer not null,
+        FOREIGN KEY(user_id) REFERENCES users(id)
+    );`;
 const create_records_table = `
-    create table if not exists records (
-        id integer primary key autoincrement,
-        memo text,
-        createdAt datetime,
-        habit_id integer,
-        foreign key(habit_id) references habits(id)
-    );`
+    CREATE TABLE records (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        memo varchar,
+        createAt datetime DEFAULT CURRENT_TIMESTAMP,
+        habit_id integer NOT NULL,
+        FOREIGN KEY(habit_id) REFERENCES habits(id)
+    );`;
 
-db.serialize(()=> {
-    db.run(create_users_table)
-    db.run(create_habits_table)
-    db.run(create_records_table)
-})
+// db.serialize(() => {
+//   db.run(create_users_table);
+//   db.run(create_habits_table);
+//   db.run(create_records_table);
+// });
 
-let getSql = ``
+let getSql = ``;
 app.get("/login", (req, res) => {
   res.render("login");
 });
 
-app.get("/register", (req, res)=> {
-    res.render('register')
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.get('/habbit_list/:id', (req, res)=> {
-    const { id } = req.params
-    getSql = `
-    select h.id, h.name AS "name", h.start_date || "~" || h.end_date AS "date", count(r.id) AS record_count
-    from users u 
-	join habits h on u.id = h.user_id 
-	join records r on r.habit_id = h.id where u.id = ${id}
-    group by h.id, h.name, h.start_date, h.end_date`
-    db.all(getSql, (err, rows)=> {
-        if(err){
-            res.status(500).send('html 500 error')
-        }else {
-            const habbits = rows
-            res.render('habbit_list', {habbits: habbits})
-        }
-    })
-    
-})
+app.get("/habit_list/:id", (req, res) => {
+  const { id } = req.params;
+  getSql = `
+    SELECT h.id, h.habit_name AS "name", h.start_date || " ~ " || h.end_date AS "date", COUNT(r.id) AS record_count, MAX(r.id) AS "record_id"
+    FROM users u 
+    JOIN habits h ON u.id = h.user_id 
+    LEFT JOIN records r ON r.habit_id = h.id 
+    WHERE u.id = ${id}
+    GROUP BY h.id, h.habit_name, h.start_date, h.end_date`;
+  db.all(getSql, (err, rows) => {
+    if (err) {
+      res.status(500).send("html 500 error");
+    } else {
+      const habits = rows;
+      res.render("habit_list", { habits: habits, user_id: id });
+    }
+  });
+});
 
-app.get('/habbit_add', (req, res)=> {
-    res.render('habbit_add')
-})
+app.get("/habit_add/:id", (req, res) => {
+  const { id } = req.params;
+  res.render("habit_add", { user_id: id });
+});
 
-app.get('/habbit_record_list', (req, res)=> {
-    res.render('habbit_record_list')
-})
+app.post("/habit_add/:id", (req, res) => {
+  const { id } = req.params;
+  const { habit_name, start_date, end_date } = req.body;
+  getSql = `
+    INSERT INTO habits(habit_name, start_date, end_date, user_id) VALUES ('${habit_name}', '${start_date}', '${end_date}', '${id}')`;
+  db.run(getSql, (err) => {
+    if (err) {
+      res.status(500).send("html 500 error");
+    } else {
+      res.redirect(`/habit_list/${id}`);
+    }
+  });
+});
 
-app.get('/habbit_record_add', (req, res)=> {
-    res.render('habbit_record_add')
-})
+app.get("/habit_delete/:user_id/:id", (req, res) => {
+  const { id, user_id } = req.params;
+  getSql = `
+    DELETE FROM habits WHERE id = ${id}`;
+  db.run(getSql, (err) => {
+    if (err) {
+      res.status(500).send("html 500 error");
+    } else {
+      res.redirect(`/habit_list/${user_id}`);
+    }
+  });
+});
+
+app.get("/habit_record_list/:id", (req, res) => {
+  const { id } = req.params; // habit_id
+  getSql = `
+  SELECT r.id AS "id", r.memo AS "memo", r.createAt AS "createAt" 
+  FROM records r
+  JOIN habits h
+  ON r.habit_id = h.id
+  WHERE r.habit_id = ${id}`;
+  db.all(getSql, (err, rows) => {
+    if (err) {
+      res.status(500).send("html 500 error");
+    } else {
+      res.render(`habit_record_list`, { records: rows, habit_id: id });
+    }
+  });
+});
+
+app.get("/habit_record_add/:id", (req, res) => {
+  const { id } = req.params;
+  res.render("habit_record_add", { habit_id: id });
+});
+
+app.post("/habit_record_add/:id", (req, res) => {
+  const { id } = req.params;
+  const { memo } = req.body;
+  getSql = `
+    INSERT INTO records(memo, habit_id) 
+    VALUES('${memo}','${id}')`;
+  db.run(getSql, (err) => {
+    if (err) {
+      res.status(500).send("html 500 error");
+    } else {
+      res.redirect(`/habit_record_list/${id}`);
+    }
+  });
+});
+
+app.get("/habit_record_delete/:habit_id/:id", (req, res) => {
+  const { id, habit_id } = req.params; // id = recode_id
+  getSql = `
+    DELETE FROM records WHERE id = ${id}`;
+  db.run(getSql, (err) => {
+    if (err) {
+      res.status(500).send("html 500 error");
+    } else {
+      res.redirect(`/habit_record_list/${habit_id}`);
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`${PORT}에서 습관관리 사이트 웹서버 실행 중`);
--- create_insert.sql
+++ create_insert.sql
@@ -1,27 +1,27 @@
-create table users(
-    id integer primary key autoincrement,
+CREATE TABLE users(
+    id integer PRIMARY KEY AUTOINCREMENT,
     name varchar,
     email varchar unique,
     password varchar,
-    createAt datetime default currnet_timestamp
+    createAt datetime DEFAULT CURRENT_TIMESTAMP
 )
 
-create table habits (
-    id integer primary key autoincrement,
+CREATE TABLE habits (
+    id integer PRIMARY KEY AUTOINCREMENT,
     habit_name varchar,
     start_date datetime,
     end_date datetime,
-    createAt datetime default currnet_timestamp,
+    createAt datetime DEFAULT CURRENT_TIMESTAMP,
     user_id integer not null,
-    foreign key(user_id) references users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id)
 )
 
-create table records (
-    id integer primary key autoincrement,
+CREATE TABLE records (
+    id integer PRIMARY KEY AUTOINCREMENT,
     memo varchar,
-    createAt datetime default currnet_timestamp,
-    habit_id integer not null,
-    foreign key(habit_id) references habits(id)
+    createAt datetime DEFAULT CURRENT_TIMESTAMP,
+    habit_id integer NOT NULL,
+    FOREIGN KEY(habit_id) REFERENCES habits(id)
 )
 
 
